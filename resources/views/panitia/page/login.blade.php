501b6cc2e87c91b3a28336091cb4ff01836497c3
Moch Wahyu Imam Santosa <imamsantosa123@gmail.com>
2017-01-04 08:15:13 +0700

    analitik

--- resources/views/panitia/page/login.blade.php
+++ resources/views/panitia/page/login.blade.php
@@ -14,6 +14,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{url('dist/css/AdminLTE.min.css')}}">
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-89811707-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
     <!-- iCheck -->
     <link rel="stylesheet" href="{{url('plugins/iCheck/square/blue.css')}}">
     <script src='https://www.google.com/recaptcha/api.js'></script>
