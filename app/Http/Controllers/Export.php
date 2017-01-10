8ce583a2a45a8e5e4592ec8f459fcb32a282d5e8
Moch Wahyu Imam Santosa <imamsantosa123@gmail.com>
2017-01-10 21:37:29 +0700

    t

--- app/Http/Controllers/Export.php
+++ app/Http/Controllers/Export.php
@@ -16,7 +16,7 @@ class Export extends Controller
     {
         $data = DataPeserta::where('status_pembayaran_id', 3)->get();
         foreach($data as $d){
-            $ceknomor = DataPeserta::where('jenis_tiket_id', $d->jenis_tiket_id)->count();
+            $ceknomor = DataPeserta::where('jenis_tiket_id', $d->jenis_tiket_id)->where('nomor_tike', '<>', null)->count();
 
             if($ceknomor <= 0){
                 $nomortiket = 1;
